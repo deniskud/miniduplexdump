0cdde63515c68626bf5def6eaba6f838fed57343
deniskud <denis.kudriakov@gmail.com>
2018-11-24 21:02:27 +0200

    add args

--- lastmini.c
+++ lastmini.c
@@ -1,18 +1,17 @@
-/*22.11.18*/
+/*init 22.11.18*/
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <lime/LimeSuite.h>
-//// special postinit function :
 int limesdr_set_channel( const unsigned int freq,
-			 const double bandwidth_calibrating,
-			 const double gain,
-			 const unsigned int channel,
-			 const char* antenna,
-			 const int is_tx,
-			 lms_device_t* device )
+  const double bandwidth_calibrating,
+  const double gain,
+  const unsigned int channel,
+  const char* antenna,
+  const int is_tx,
+  lms_device_t* device )
 {// anth1
   int nb_antenna = LMS_GetAntennaList(device, is_tx, channel, NULL);
   lms_name_t list[ nb_antenna ];
@@ -52,8 +51,7 @@ int limesdr_init( const double sample_rate,
        const char* antenna,
        const int is_tx,
        lms_device_t** device,
-       double* host_sample_rate
-){
+       double* host_sample_rate){
   int device_count = LMS_GetDeviceList(NULL);
   lms_info_str_t device_list[ device_count ];
   LMS_GetDeviceList(device_list);
@@ -68,8 +66,22 @@ int limesdr_init( const double sample_rate,
   limesdr_set_channel( freq, bandwidth_calibrating, gain, channel, antenna, is_tx, *device );  
   return 0;
 }/////////////////////////////////////////////////////////////////////
-int main(){
+int main(int argc, char** argv){
+  if ( argc < 2 ) {
+		printf("--usage: %s <OPTIONS>\n", argv[0]);
+		printf("  -f <FREQUENCY>\n"
+		       "  -b <BANDWIDTH_CALIBRATING> (default: 8e6)\n"
+		       "  -s <SAMPLE_RATE> (default: 5e6)\n"
+		       "  -g <GAIN_dB> range [0, 73] (default: 0)\n"
+           "  -l <BUFFER_SIZE>  (default: 1000*1000)\n"
+		       "  -a <ANTENNA> (LNAL | LNAH | LNAW) (default: LNAW)\n"
+		       "  -o <OUTPUT_FILENAME CH A>\n"
+		       "  -p <OUTPUT_FILENAME CH B>\n"		       
+           "  -n NUMBER of sampes\n");
+		return 1;
+	}	
   unsigned int freq = 2000000000;
+  long long int maxnumber = 0;
   double bandwidth_calibrating = 8e6;
   double host_sample_rate = 5.0;
   double sample_rate = 5000000;
@@ -78,10 +90,23 @@ int main(){
   unsigned int device_i = 0;
   unsigned int channel = 0; // anything  
   char* antenna = "LNAW";
+	char* output_filename1 = "line-a.iq";  
+	char* output_filename2 = "line-b.iq";  
   int nb_samples=0;
   int nb_samples2=0;
   lms_stream_meta_t meta;
   lms_device_t* device1 = NULL;
+ 	for ( int i = 1; i < argc-1; i += 2 ) {
+    if      (strcmp(argv[i], "-f") == 0) { freq = atof( argv[i+1] ); }
+    else if (strcmp(argv[i], "-b") == 0) { bandwidth_calibrating = atof( argv[i+1] ); }
+    else if (strcmp(argv[i], "-s") == 0) { sample_rate = atof( argv[i+1] ); }
+    else if (strcmp(argv[i], "-g") == 0) { gain = atof( argv[i+1] ); }
+    else if (strcmp(argv[i], "-l") == 0) { buffer_size = atoi( argv[i+1] ); }
+    else if (strcmp(argv[i], "-a") == 0) { antenna = argv[i+1]; }
+    else if (strcmp(argv[i], "-o") == 0) { output_filename1 = argv[i+1]; }
+    else if (strcmp(argv[i], "-p") == 0) { output_filename2 = argv[i+1]; }
+    else if (strcmp(argv[i], "-n") == 0) { maxnumber = atof( argv[i+1] ); }
+	}    
   struct s16iq_sample_s {  //definition bbufers
     short i;
     short q;
@@ -109,11 +134,10 @@ int main(){
   LMS_StartStream(&rx_stream2);
   FILE* fd1;
   FILE* fd2;
-  fd1 = fopen( "mimo-a.iq", "w+b" );
-  fd2 = fopen( "mimo-b.iq", "w+b" );
+  fd1 = fopen( output_filename1, "w+b" );
+  fd2 = fopen( output_filename2, "w+b" );
   if ( fd1 == NULL || fd2 == NULL ) perror("fopen()");
-  int maxsize = 5; //     =1sec 
-  while (maxsize--!=0){ /////////////////// start main loop                 
+  while (maxnumber--!=0){ /////////////////// start main loop                 
     nb_samples = LMS_RecvStream( &rx_stream, buff1, buffer_size, &meta, 100 );
     nb_samples2 = LMS_RecvStream( &rx_stream2, buff2, buffer_size, &meta, 100 );
     fwrite( buff1, sizeof( *buff1 ), nb_samples, fd1 );
@@ -127,6 +151,7 @@ int main(){
   LMS_DestroyStream(device1, &rx_stream2);
   free( buff1 );
   free( buff2 );
+  printf("Files :\n %s\n %s\nis wited and closed.\n",output_filename1,output_filename2);
   LMS_Close(device1);
   fclose( fd1 );
   fclose( fd2 );
