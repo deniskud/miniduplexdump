e76820c3937a0d3d8418d86095fedd3fbd557414
josper <svstas@gmail.com>
2019-03-04 19:42:13 +0200

    maximum minimized

--- lastmini.c
+++ lastmini.c
@@ -8,76 +8,11 @@
 
 const long int frq=100000;//100KHz
 const double base=.2;//100KHz
-
-
 const double   PI=3.14159265358979323846;
 const double   PI2=3.14159265358979323846*2;
 const double   PI_2=3.14159265358979323846/2;
 const long int c=299792458;//     (m/sec)
-
-int limesdr_set_channel( const unsigned int freq,
-  const double bandwidth_calibrating,
-  const double gain,
-  const unsigned int channel,
-  const char* antenna,
-  const int is_tx,
-  lms_device_t* device )
-{// anth1
-  int nb_antenna = LMS_GetAntennaList(device, is_tx, channel, NULL);
-  lms_name_t list[ nb_antenna ];
-  LMS_GetAntennaList( device, is_tx, channel, list );
-  int antenna_found = 0;
-  int i;
-  for ( i = 0; i < nb_antenna; i++ ) {
-    if ( strcmp( list[i], antenna ) == 0 ) {
-      antenna_found = 1;
-      LMS_SetAntenna( device, is_tx, channel, i );			
-    }
-  }// anth2
-  nb_antenna = LMS_GetAntennaList(device, is_tx, 1-channel, NULL);
-  lms_name_t list2[ nb_antenna ];
-  LMS_GetAntennaList( device, is_tx, 1-channel, list2 );
-  antenna_found = 0;
-  for ( i = 0; i < nb_antenna; i++ ) {
-    if ( strcmp( list[i], antenna ) == 0 ) {
-      antenna_found = 1;
-      LMS_SetAntenna( device, is_tx, 1-channel, i );			
-    }
-  }//postinit
-  LMS_Calibrate( device, is_tx, channel, bandwidth_calibrating, 0 );	 
-  LMS_Calibrate( device, is_tx, 1-channel, bandwidth_calibrating, 0 );	 
-  LMS_SetLOFrequency( device, is_tx, channel, freq);
-  LMS_SetLOFrequency( device, is_tx, 1-channel, freq);
-  LMS_SetGaindB( device, is_tx, channel, gain );
-  LMS_SetGaindB( device, is_tx, 1-channel, gain );
-  return 0;
-}///// special init function
-
-int limesdr_init( const double sample_rate,
-       const unsigned int freq,
-       const double bandwidth_calibrating,
-       const double gain,
-       const unsigned int device_i,
-       const unsigned int channel,
-       const char* antenna,
-       const int is_tx,
-       lms_device_t** device,
-       double* host_sample_rate){
-  int device_count = LMS_GetDeviceList(NULL);
-  lms_info_str_t device_list[ device_count ];
-  LMS_GetDeviceList(device_list);
-  LMS_Open(device, device_list[ device_i ], NULL);
-  LMS_Reset(*device);
-  LMS_Init(*device);
-  int is_not_tx = (is_tx == LMS_CH_TX) ? LMS_CH_RX : LMS_CH_TX;	
-  LMS_EnableChannel(*device, LMS_CH_RX,   1-channel, true);
-  LMS_EnableChannel(*device, LMS_CH_RX,   channel,   true);
-  LMS_SetSampleRate( *device, sample_rate, 0 );
-  LMS_GetSampleRate( *device, is_tx, channel, host_sample_rate, NULL );
-  limesdr_set_channel( freq, bandwidth_calibrating, gain, channel, antenna, is_tx, *device );  
-  return 0;
-}/////////////////////////////////////////////////////////////////////
-
+/////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv){
   if ( argc < 2 ) {
     printf("--usage: %s <OPTIONS>\n", argv[0]);
@@ -88,11 +23,11 @@ int main(int argc, char** argv){
            "  -l <BUFFER_SIZE>  (default: 1000*1000)\n"
            "  -a <ANTENNA> (LNAL | LNAH | LNAW) (default: LNAW)\n"
            "  -o <OUTPUT_FILENAME CH A>\n"
-           "  -p <OUTPUT_FILENAME CH B>\n"		       
+           "  -p <OUTPUT_FILENAME CH B>\n"
            "  -n NUMBER of sampes\n"
-           "  -m for m=1: mix@ stderr(I1,Q1,I2,Q2,I1,Q1,I2,Q2...)");   
+           "  -m for m=1: mix@ stderr(I1,Q1,I2,Q2,I1,Q1,I2,Q2...)\n");
     return 1;
-  }	
+  }
   unsigned int freq = 2000000000;
   long long int maxnumber = 0;
   double bandwidth_calibrating = 2500000;
@@ -101,14 +36,14 @@ int main(int argc, char** argv){
   double gain = 30;
   unsigned int buffer_size = 1000*1000;
   unsigned int device_i = 0;
-  unsigned int channel = 0; // anything  
+  unsigned int channel = 0; // anything
   char* antenna = "LNAH";
-  char* output_filename1 = "line-a.iq";  
-  char* output_filename2 = "line-b.iq";  
+  size_t anta = 2; //  1-RX LNA_H port   2-RX LNA_L port  3-RX LNA_W port
+  size_t antb = 2; //  1-RX LNA_H port   2-RX LNA_L port  3-RX LNA_W port
+  char* output_filename1 = "line-a.iq";
+  char* output_filename2 = "line-b.iq";
   int nb_samples=0;
   int nb_samples2=0;
-  lms_stream_meta_t meta;
-  lms_device_t* device1 = NULL;
   int alg=0;
   for ( int i = 1; i < argc-1; i += 2 ) {
     if      (strcmp(argv[i], "-f") == 0) { freq = atof( argv[i+1] ); }
@@ -120,17 +55,40 @@ int main(int argc, char** argv){
     else if (strcmp(argv[i], "-o") == 0) { output_filename1 = argv[i+1]; }
     else if (strcmp(argv[i], "-p") == 0) { output_filename2 = argv[i+1]; }
     else if (strcmp(argv[i], "-n") == 0) { maxnumber = atof( argv[i+1] ); }
-    else if (strcmp(argv[i], "-m") == 0) { alg = atof( argv[i+1] ); }  
-  }    
+    else if (strcmp(argv[i], "-m") == 0) { alg = atof( argv[i+1] ); }
+  }
+  lms_stream_meta_t meta;
+  lms_device_t* device = NULL;
   struct s16iq_sample_s {  //definition bbufers
     short i;
     short q;
   } __attribute__((packed));
   struct s16iq_sample_s *buff1 = (struct s16iq_sample_s*)malloc(sizeof(struct s16iq_sample_s) * buffer_size);
-  struct s16iq_sample_s *buff2 = (struct s16iq_sample_s*)malloc(sizeof(struct s16iq_sample_s) * buffer_size);  
-  struct s16iq_sample_s *mixbuf = (struct s16iq_sample_s*)malloc(sizeof(struct s16iq_sample_s) * buffer_size * 2);  
+  struct s16iq_sample_s *buff2 = (struct s16iq_sample_s*)malloc(sizeof(struct s16iq_sample_s) * buffer_size);
+  struct s16iq_sample_s *mixbuf = (struct s16iq_sample_s*)malloc(sizeof(struct s16iq_sample_s) * buffer_size * 2);
   printf("\nfreq=%d\nsamplerate=%f\ngain=%f\nbufer size=%d\nalg=%d\n",freq,sample_rate,gain,buffer_size,alg);
-  limesdr_init( sample_rate,freq,bandwidth_calibrating,gain,device_i,0,antenna,LMS_CH_RX,&device1,&host_sample_rate);
+
+  int device_count = LMS_GetDeviceList(NULL);
+  lms_info_str_t device_list[ device_count ];
+  LMS_GetDeviceList(device_list);
+  LMS_Open(&device, device_list[ device_i ], NULL);
+
+  LMS_Reset(device); //   reset
+  LMS_Init(device);  //  default
+
+  LMS_EnableChannel(device, LMS_CH_RX, 0, true);
+  LMS_EnableChannel(device, LMS_CH_RX, 1, true);
+  LMS_SetSampleRate(device, sample_rate, 0);
+  LMS_GetSampleRate(device, LMS_CH_RX, channel, &host_sample_rate, NULL );
+  LMS_SetAntenna( device, LMS_CH_RX, 0, anta );
+  LMS_SetAntenna( device, LMS_CH_RX, 1, antb );
+  LMS_Calibrate( device, LMS_CH_RX, 0, bandwidth_calibrating, 0 );
+  LMS_Calibrate( device, LMS_CH_RX, 1, bandwidth_calibrating, 0 );
+  LMS_SetLOFrequency( device, LMS_CH_RX, 0, freq);
+  LMS_SetLOFrequency( device, LMS_CH_RX, 1, freq);
+  LMS_SetGaindB( device, LMS_CH_RX, 0, gain );
+  LMS_SetGaindB( device, LMS_CH_RX, 1, gain );
+
   lms_stream_t rx_stream = {
     .channel = 0,
     .fifoSize = buffer_size * sizeof(*buff1),
@@ -145,23 +103,23 @@ int main(int argc, char** argv){
     .isTx = LMS_CH_RX,
     .dataFmt = LMS_FMT_I16
   };
-  LMS_SetupStream(device1, &rx_stream);
-  LMS_SetupStream(device1, &rx_stream2);
+  LMS_SetupStream(device, &rx_stream);
+  LMS_SetupStream(device, &rx_stream2);
   LMS_StartStream(&rx_stream);
   LMS_StartStream(&rx_stream2);
+
   FILE* fd1;
   FILE* fd2;
   FILE* fd = stderr;
-  
+
   double defi;
   double gamma;
   double atg1;
   double atg2;
-  
   double deltaphi(short i1,short q1,short i2,short q2){
     atg1=atan(i1/q1);
-    atg2=atan(i2/q2); 
-    if (atg1>atg2) {return (atg1-atg2);} else {return(PI2+atg1-atg2);} 
+    atg2=atan(i2/q2);
+    if (atg1>atg2) {return (atg1-atg2);} else {return(PI2+atg1-atg2);}
   }
   double azimut(long int frq, double deltaphase, double base){
 //    double dl=c*deltaphase/(frq*PI2); // time=deltaphase/(frq*PI2); dl=c*time
@@ -174,7 +132,7 @@ int main(int argc, char** argv){
     if ( fd1 == NULL || fd2 == NULL ) perror("fopen()");
   }
   int pointer = 0;
-  while (maxnumber--!=0){ /////////////////// start main loop                 
+  while (maxnumber--!=0){ /////////////////// start main loop
     if (alg==0){
       nb_samples = LMS_RecvStream( &rx_stream, buff1, buffer_size, &meta, 100 );
       nb_samples2 = LMS_RecvStream( &rx_stream2, buff2, buffer_size, &meta, 100 );
@@ -182,10 +140,10 @@ int main(int argc, char** argv){
       fwrite( buff2, sizeof( *buff2 ), nb_samples2, fd2 );
       fflush( fd2 );
       fflush( fd1 );
-    }   
+    }
     if (alg==1){
       nb_samples = LMS_RecvStream( &rx_stream, buff1, buffer_size, &meta, 100 );
-      nb_samples2 = LMS_RecvStream( &rx_stream2, buff2, buffer_size, &meta, 100 );    
+      nb_samples2 = LMS_RecvStream( &rx_stream2, buff2, buffer_size, &meta, 100 );
       for (pointer=1;pointer<=buffer_size;pointer++){
         mixbuf[pointer*2]   = buff2[pointer];
         mixbuf[pointer*2-1] = buff1[pointer];
@@ -201,23 +159,23 @@ int main(int argc, char** argv){
       fflush( fd2 );
       fflush( fd1 );
       for (pointer=1;pointer<=buffer_size;pointer++){
-        if (buff1[pointer].q!=0 && buff2[pointer].q!=0){ 
-          atg1=atan(buff1[pointer].i/buff1[pointer].q); 
-          atg2=atan(buff2[pointer].i/buff2[pointer].q); 
+        if (buff1[pointer].q!=0 && buff2[pointer].q!=0){
+          atg1=atan(buff1[pointer].i/buff1[pointer].q);
+          atg2=atan(buff2[pointer].i/buff2[pointer].q);
         }
-        if (atg1>atg2) {defi=atg1-atg2;} else {defi=PI2+atg1-atg2;} 
-        gamma=asin(c*defi/(frq*PI2*base))*180/PI; //gamma=(PI_2-acos(c*defi/(frq*PI2*base)))*180/PI;  
-        printf("%f\n",gamma);      
+        if (atg1>atg2) {defi=atg1-atg2;} else {defi=PI2+atg1-atg2;}
+        gamma=asin(c*defi/(frq*PI2*base))*180/PI; //gamma=(PI_2-acos(c*defi/(frq*PI2*base)))*180/PI;
+        printf("%f\n",gamma);
       }
-    }       
+    }
   }/////////////////// end main loop
   LMS_StopStream(&rx_stream);
   LMS_StopStream(&rx_stream2);
-  LMS_DestroyStream(device1, &rx_stream);
-  LMS_DestroyStream(device1, &rx_stream2);
+  LMS_DestroyStream(device, &rx_stream);
+  LMS_DestroyStream(device, &rx_stream2);
   free( buff1 );
   free( buff2 );
-  LMS_Close(device1);
+  LMS_Close(device);
   if (alg==1) fclose( fd );
   if (alg==0){
     fclose( fd1 );
